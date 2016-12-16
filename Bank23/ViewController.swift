41c6565949575e8e78a1fc089f45b94cc7317a9e
Ian Vonseggern <ianv@fb.com>
2016-12-16 17:33:50 -0500

    Initial Commit

---
+++ Bank23/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Bank23
+//
+//  Created by Ian Vonseggern on 12/16/16.
+//  Copyright © 2016 Ian Vonseggern. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
